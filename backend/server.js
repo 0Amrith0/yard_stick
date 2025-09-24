cbba407cceb826999db4b32708996397acb98339
0Amrith0 <133968896+0Amrith0@users.noreply.github.com>
2025-09-24 20:43:19 +0530

    server file changed

--- backend/server.js
+++ backend/server.js
@@ -33,10 +33,10 @@ app.use("/api/health", healthRoutes);
 app.use("/api/tenants", tenantRoutes)
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../../frontend/dist")));
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
   app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "../../frontend/dist/index.html"));
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
   });
 }
 
